18f47f70f7dc8020bee10cdad9aea66cfccfd40c
NDT0 <ndt.080@yandex.by>
2020-08-25 17:06:40 +0300

    UPDATE: add loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app-loader">
+        <div class="d-flex align-items-center">
+            <strong>Loading...</strong>
+            <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Loader"
+    }
+</script>
+
+<style scoped>
+
+</style>
